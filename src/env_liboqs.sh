c90422886844c61d7ca09f0149961de8f6d1901d
finslis-core <torokfinalis@gmail.com>
2026-05-16 08:19:42 +0400

    reccorect install/build

---
+++ src/env_liboqs.sh
@@ -0,0 +1,2 @@
+export PKG_CONFIG_PATH="/home/greendragon/Desktop/coin/src/liboqs/install/lib/pkgconfig"
+export LD_LIBRARY_PATH="/home/greendragon/Desktop/coin/src/liboqs/install/lib:${LD_LIBRARY_PATH:-}"
